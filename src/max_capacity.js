6e5ece90cf4b0d2e714294fa70e1d8cd06d5e870
Moquan Chen <mq.chen@gmail.com>
2013-03-25 17:16:05 +0100

    Added support for more restricting subranges

--- src/max_capacity.js
+++ src/max_capacity.js
@@ -1,7 +1,7 @@
 (function(exports){
 
     var Capacity = function() {
-        this.addRestrictions = new Array();
+        this.allRestrictions = new Array();
         this.restrictions = new Array();
     };
 
@@ -12,14 +12,42 @@
             "capacity" : cap
         };
 
-        this.allRestrictions.push(restriction);
-
         // Only add one is more restrictive
+        if(this.restrictions.length == 0) this.restrictions.push(restriction);
+        else {
 
+            // Is it non-intersecting with all
+            var superRangeIndex = -1;
+            for(var i = 0; i < this.restrictions.length; i++) {
+                // Not subrange, add it
+                if(this.isSubRange(this.restrictions[i], restriction)) {
+                    superRangeIndex = i;
+                    break;
+                }
+                // Is subrange, and more restrictive, replace previous one
+                // else if(this.isMoreRestrictive(this.restrictions[i], restriction)) {
+                //         this.restrictions[i] = restriction;
+                // }
+            }
+            if(superRangeIndex < 0) {
+                this.restrictions.push(restriction);
+            }
+            else if(this.isMoreRestrictive(this.restrictions[superRangeIndex], restriction)) {
+                // It is subrange, replace the more lenient subrange if this is more restrictive
+                this.restrictions[superRangeIndex] = restriction;
+            }
+        }
+
+        // Add for reference
+        this.allRestrictions.push(restriction);
     };
 
+    Capacity.prototype.isSubRange = function(range, maybeSubRange) {
+        return maybeSubRange.fromIncl >= range.fromIncl && maybeSubRange.toIncl <= range.toIncl;
+    }
+
     Capacity.prototype.isMoreRestrictive = function(orgRestrict, newRestrict) {
-        
+        return this.isSubRange(orgRestrict, newRestrict) && newRestrict.capacity <= orgRestrict.capacity;
     };
 
     Capacity.prototype.getMaxCapacity = function() {
--- test/max_capacity-test.js
+++ test/max_capacity-test.js
@@ -25,12 +25,32 @@ buster.testCase("Max capacity test", {
             assert.equals(this.capacity.getMaxCapacity(), 200);
         },
 
-        "should find max capacity with 2 restrictions, where one is a more restrictive a sub-range" : function() {
-            this.capacity.addRestriction(1, 10, 100);
-            this.capacity.addRestriction(1, 5, 100);
+        "should find max capacity with 3 restrictions, 1 more restrictive subrange of another" : function() {
+            this.capacity.addRestriction(1, 6, 100);
+            this.capacity.addRestriction(7, 10, 100);
+            this.capacity.addRestriction(1, 5, 100); // This should be considered
 
-            assert.equals(this.capacity.getMaxCapacity(), 100);
-        }
+            assert.equals(this.capacity.getMaxCapacity(), 200);
+        },
+
+        "should find max capacity with 4 restrictions, 1 more restrictive subrange of another, and 1 more lenient subrange" : function() {
+            this.capacity.addRestriction(1, 6, 100);
+            this.capacity.addRestriction(7, 10, 100);
+            this.capacity.addRestriction(1, 5, 100); // This should be considered
+            this.capacity.addRestriction(8, 10, 200); // This should be ignored
+
+            assert.equals(this.capacity.getMaxCapacity(), 200);
+        },
+
+        "should find max capacity with many restrictions, all non-intersecting" : function() {
+            var total = 0;
+            for(var i = 1; i <= 10; i++) {
+                total += 100;
+                this.capacity.addRestriction(i, i, 100);
+            }
+
+            assert.equals(this.capacity.getMaxCapacity(), total);
+        },
     }
 
 });
