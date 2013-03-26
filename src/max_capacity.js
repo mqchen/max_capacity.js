8e2fe59783f94e945b728adc50780c89d6120be4
Moquan Chen <mq.chen@gmail.com>
2013-03-26 11:32:20 +0100

    Added support for getMaxCapacity with query range

--- src/max_capacity.js
+++ src/max_capacity.js
@@ -163,116 +163,16 @@
         return "nonintersecting";
     };
 
-/*
-    Capacity.prototype.addRestriction = function(fromIncl, toIncl, cap) {
-        var restriction = this.createRestriction(fromIncl, toIncl, cap);
 
-        if(this.restrictions.length == 0) this.restrictions.push(restriction);
-        else {
-            // Is it non-intersecting with all other active restrictions?
-            var superRangeIndex = this.isSubRangeOfAll(this.restrictions, restriction);
-            // Not subrange
-            if(superRangeIndex < 0) {
-                var overlapsIds = this.getOverlappingRanges(this.restrictions, restriction);
-                if(overlapsIds.length > 0) { // BUG
-                    // Restriction is exclusive (does not affect others)
-                    this.restrictions.push(restriction);
-                }
-            }
-            // Is subrange, add it
-            else if(this.isMoreRestrictive(this.restrictions[superRangeIndex], restriction)) {
-                this._addSubRange(superRangeIndex, restriction);
-            }
-        }
+    Capacity.prototype.getMaxCapacity = function(fromIncl, toIncl) {
+        fromIncl = (fromIncl === undefined) ? Number.MIN_VALUE : fromIncl;
+        toIncl = (toIncl === undefined) ? Number.MAX_VALUE : toIncl;
+        var queryRange = this.createRestriction(fromIncl, toIncl);
 
-        // Add for reference
-        this.allRestrictions.push(restriction);
-    };
-/*
-    Capacity.prototype._addSubRange = function(superRangeIndex, subRange) {
-        if(this.isMoreRestrictive(this.restrictions[superRangeIndex], subRange)) {
-            // It is subrange, replace/resize/split the more lenient subrange if this is more restrictive
-            var superRange = this.restrictions[superRangeIndex];
-
-            // Remove superrange, and add subrange
-            this.restrictions[superRangeIndex] = subRange;
-
-            // Part of superrange before subrange
-            var prefixRange = this.createRestriction(superRange.fromIncl, subRange.fromIncl);
-            var suffixRange = this.createRestriction(subRange.toIncl, superRange.toIncl);
-            
-            // Calc capacity of prefix range
-
-            // Both prefix and suffix are valid
-            if(prefixRange.fromIncl < prefixRange.toIncl && suffixRange.fromIncl < suffixRange.toIncl) {
-                var capacity = (superRange.capacity - subRange.capacity) / 2;
-                prefixRange.capacity = capacity;
-                suffixRange.capacity = capacity;
-
-                this.restrictions.push(prefixRange);
-                this.restrictions.push(suffixRange);
-            }
-            // Only prefix range valid
-            else if(prefixRange.fromIncl < prefixRange.toIncl) {
-                var capacity = superRange.capacity - subRange.capacity;
-                prefixRange.capacity = capacity;
-
-                this.restrictions.push(prefixRange);
-            }
-            // Only suffix range valid
-            else if(suffixRange.fromIncl < suffixRange.toIncl) {
-                var capacity = superRange.capacity - subRange.capacity;
-                suffixRange.capacity = capacity;
-
-                this.restrictions.push(suffixRange);
-            }
-        }
-    };
-*/
-/*
-    Capacity.prototype.getOverlappingRanges = function(restrictions, maybeOverlapping) {
-        
-        var overlapsIds = new Array();
-
-        for(var i = 0; i < restrictions.length; i++) {
-            if(this.isOverlappingRangeButNotSubRange(restrictions[i], maybeOverlapping)) {
-                overlapsIds.push(i);
-            }
-        }
-
-        return overlapsIds;
-    };
-
-    Capacity.prototype.isOverlappingRangeButNotSubRange = function(range, maybeOverlapping) {
-        return !this.isSubRange(range, maybeOverlapping)
-            && (range.fromIncl < maybeOverlapping.fromIncl && range.toIncl < maybeOverlapping.toIncl)
-            || (maybeOverlapping.fromIncl < range.fromIncl && maybeOverlapping.toIncl < range.toIncl);
-    };
-
-    Capacity.prototype.isSubRangeOfAll = function(restrictions, maybeSubRange) {
-        var superRangeIndex = -1;
-        for(var i = 0; i < restrictions.length; i++) {
-            if(this.isSubRange(restrictions[i], maybeSubRange)) {
-                superRangeIndex = i;
-                break;
-            }
-        }
-        return superRangeIndex;
-    };
-
-    Capacity.prototype.isSubRange = function(range, maybeSubRange) {
-        return maybeSubRange.fromIncl >= range.fromIncl && maybeSubRange.toIncl <= range.toIncl;
-    }
-
-    Capacity.prototype.isMoreRestrictive = function(orgRestrict, newRestrict) {
-        return this.isSubRange(orgRestrict, newRestrict) && newRestrict.capacity <= orgRestrict.capacity;
-    };
-*/
-    Capacity.prototype.getMaxCapacity = function() {
         var max = 0;
 
-        for(var i in this.restrictions) {
-            if(this.restrictions.hasOwnProperty(i)) {
+        for(var i = 0; i < this.restrictions.length; i++) {
+            if(this.isIntersecting(this.restrictions[i], queryRange)) {
                 max += this.restrictions[i].capacity;
             }
         }
--- test/max_capacity-test.js
+++ test/max_capacity-test.js
@@ -9,56 +9,6 @@ buster.testCase("Max capacity test", {
     setUp : function() {
         this.capacity = new Capacity();
     },
-/*
-    "isOverlappingRangeButNotSubRange" : {
-        "should only be true if one overlaps another, without being subrange" : function() {
-
-            var r1 = {
-                "fromIncl" : 3,
-                "toIncl" : 10,
-                "capacity" : 100
-            };
-
-            var r2 = {
-                "fromIncl" : 5,
-                "toIncl" : 11,
-                "capacity" : 100
-            };
-
-            var r3 = {
-                "fromIncl" : 1,
-                "toIncl" : 5,
-                "capacity" : 100  
-            }
-
-            var r4 = { // subrange of r1
-                "fromIncl" : 5,
-                "toIncl" : 7,
-                "capacity" : 100  
-            }
-
-            assert.equals(this.capacity.isOverlappingRangeButNotSubRange(r1, r2), true);
-            assert.equals(this.capacity.isOverlappingRangeButNotSubRange(r1, r3), true);
-            assert.equals(this.capacity.isOverlappingRangeButNotSubRange(r2, r3), true);
-            assert.equals(this.capacity.isOverlappingRangeButNotSubRange(r1, r4), false);
-        }
-    },
-
-    "getOverlappingRanges" : {
-        "should find overlapping range" : function() {
-            this.capacity.addRestriction(1, 10, 100); // Should find this
-            this.capacity.addRestriction(1, 4, 100); // Should not find this
-            this.capacity.addRestriction(12, 14, 100); // Should not find this
-            var overlapping = {
-                "fromIncl" : 5,
-                "toIncl" : 11,
-                "capacity" : 100
-            };
-
-            assert.equals(this.capacity.getOverlappingRanges(this.capacity.restrictions, overlapping), [0]);
-        }
-    },
-*/
 
     "getIntersectingRestrictions" : {
         "should find intersecting restrictions" : function() {
@@ -255,6 +205,15 @@ buster.testCase("Max capacity test", {
 
             assert.equals(this.capacity.getMaxCapacity(), 200);
         },
+
+        "should get max capacity within given range" : function() {
+            this.capacity.addRestriction(1, 6, 100);
+            this.capacity.addRestriction(7, 10, 100);
+            this.capacity.addRestriction(1, 5, 100); // This should be considered
+            this.capacity.addRestriction(8, 10, 200); // This should be ignored
+
+            assert.equals(this.capacity.getMaxCapacity(1, 2), 100);
+        }
     }
 
 });
